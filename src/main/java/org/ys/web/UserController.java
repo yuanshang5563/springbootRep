2318c829c86e202eb6f053607a59e317a50f1b70
yuanshang5563 <1137422989@qq.com>
2018-09-02 00:50:40 +0800

    aa

---
+++ src/main/java/org/ys/web/UserController.java
@@ -0,0 +1,29 @@
+package org.ys.web;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ys.model.CoreUser;
+import org.ys.service.CoreUserService;
+
+@RestController
+@RequestMapping("/UserController.do")
+public class UserController {
+	@Autowired
+	private CoreUserService coreUserService;
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		System.out.println("hello");
+		CoreUser user = new CoreUser();
+		user.setUserName("test");
+		user.setBirtyday(new Date());
+		user.setPassword("123456");
+		coreUserService.save(user);
+		return "hello";
+		
+		//return new ModelAndView("user_list");
+	}
+}
